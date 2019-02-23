2aab142f1421c2e8ac53f83cefb6fe0fe63f32ca
Rob Howell <rob@robhowell.com>
2019-02-23 19:25:33 +0000

    Replace jetpack with webpack

--- scripts/setMainScript.js
+++
@@ -1,9 +0,0 @@
-const editJsonFile = require('edit-json-file');
-const manifest = require('../dist/assets/manifest.json');
-
-const mainScript = manifest['bundle.js'];
-const rootRelativeMainScript = `dist${mainScript}`;
-const packageJsonFile = editJsonFile(`${__dirname}/../package.json`);
-
-packageJsonFile.set('main', rootRelativeMainScript);
-packageJsonFile.save();
