1bcd4eebba6441eff2de06fec2c1191637eca113
Rob Howell <rob@robhowell.com>
2019-02-23 19:18:58 +0000

    Add named-export filtering to findUsage

--- scripts/findUsage/findUsage.js
+++ scripts/findUsage/findUsage.js
@@ -1,10 +1,33 @@
 const path = require('path');
 
 const gitCloneRepo = require('./gitCloneRepo');
+const { Parser } = require('acorn');
 const { readdir, readFile, lstat } = require('./fs');
 const { TMP_DIRECTORY } = require('./config');
 
-async function searchFilesInDirectoryAsync(dir, filter, ext) {
+const JsxParser = Parser.extend(require('acorn-jsx')());
+const parseJsxModule = content =>
+  JsxParser.parse(content, { ecmaVersion: 10, sourceType: 'module' });
+
+const isImportOfModule = moduleName => node =>
+  node.type === 'ImportDeclaration' && node.source.value === moduleName;
+
+const getNamedImports = importNodes =>
+  importNodes
+    .reduce((prev, importNode) => {
+      return [
+        ...prev,
+        importNode.specifiers
+          .filter(node => node.type === 'ImportSpecifier')
+          .reduce((prev, specifier) => [...prev, specifier.imported.name], [])
+      ];
+    }, [])
+    .flat(100);
+
+const getSourceFilename = localFilename =>
+  localFilename.replace(`${TMP_DIRECTORY.replace('./', '')}/`, '');
+
+async function searchFilesInDirectoryAsync(dir, moduleName, ext) {
   try {
     await readdir(dir);
   } catch (err) {
@@ -13,13 +36,29 @@ async function searchFilesInDirectoryAsync(dir, filter, ext) {
 
   const allFiles = await getFilesInDirectoryAsync(dir, ext);
 
-  return allFiles.reduce(async (prevPromise, file) => {
+  return allFiles.reduce(async (prevPromise, localFilename) => {
     const prev = await prevPromise;
-    const fileContent = await readFile(file);
-    const regex = new RegExp(filter);
+    const fileContent = await readFile(localFilename);
+    const regex = new RegExp(moduleName);
 
     if (regex.test(fileContent)) {
-      return [...prev, file];
+      const fileAst = parseJsxModule(fileContent);
+      const importsOfTargetModule = fileAst.body.filter(
+        isImportOfModule(moduleName)
+      );
+
+      if (importsOfTargetModule.length) {
+        const filename = getSourceFilename(localFilename);
+        const namedImports = getNamedImports(importsOfTargetModule);
+
+        return [
+          ...prev,
+          {
+            filename,
+            namedImports
+          }
+        ];
+      }
     }
 
     return prev;
@@ -57,11 +96,11 @@ const main = async () => {
 
   const filesFound = await searchFilesInDirectoryAsync(
     TMP_DIRECTORY,
-    new RegExp('@rth/component-library'),
+    '@rth/component-library',
     '.js'
   );
 
-  console.log(JSON.stringify(filesFound));
+  console.log(filesFound);
 };
 
 main();
